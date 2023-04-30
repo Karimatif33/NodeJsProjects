ee90e6ec81e72d49d8cc822ca1533bc8a0ffc203
Karim Atif <101069884+Karimatif33@users.noreply.github.com>
2023-04-30 08:22:19 +0300

    EshopeNodeJsProject(5)

--- E-Commerce Web API/app.js
+++ E-Commerce Web API/app.js
@@ -4,55 +4,23 @@ const port = 3000
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-
-// Middleware
-app.use(express.json())
-app.use(morgan('tiny'))
-
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true,
-    },
-})
-
-const Product = mongoose.model('Product', productSchema)
-
 require('dotenv/config')
-
 const api = process.env.API_URL
 
-app.get(`${api}/products`, async (req, res) => {
-    const productList = await Product.find()
-    if (!productList) {
-        res.status(500).json({
-            success: false,
-        })
-    }
-    res.send(productList)
-})
+const productsRouter = require('./routers/product')
+// Middleware
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock,
-    })
-    product
-        .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct)
-            console.log('Product created successfully')
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false,
-            })
-        })
-})
+// Routers
+app.use(`${api}/products`,productsRouter)
+
+
+
+
+
+
+
 
 mongoose
     .connect(process.env.CONNECTION_STRING)
