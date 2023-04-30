c89f0c75c5055deaeb6dd4fe2e6b13342d92d19d
Karim Atif <101069884+Karimatif33@users.noreply.github.com>
2023-04-30 22:31:10 +0300

    EshopeNodeJsProject(9)

--- E-Commerce Web API/models/category.js
+++ E-Commerce Web API/models/category.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
 const categorySchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true,
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: ''
     },
 })
 exports.Category = mongoose.model('Category', categorySchema)
--- E-Commerce Web API/routers/categories.js
+++ E-Commerce Web API/routers/categories.js
@@ -3,32 +3,53 @@ const express = require('express')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find()
-    if (!productList) {
+    const categoryList = await Category.find()
+    if (!categoryList) {
         res.status(500).json({
             success: false,
         })
     }
-    res.send(productList)
+    res.status(200).send(categoryList)
+})
+router.get(`/:id`, async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (!category) {
+        res.status(500).json({
+            success: false,
+            message: 'the category the given Id was not found',
+        })
+    }
+    res.status(200).send(category)
+})
+router.post(`/`, async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+        // image: req.body.image
+    })
+    category = await category.save()
+    if (!category)
+        return res.status(404).send('the category cannot be created!')
+    res.send(category)
 })
 
-router.post(`/`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock,
-    })
-    product
-        .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct)
-            console.log('Product created successfully')
+router.delete(`/:id`, (req, res) => {
+    Category.findByIdAndRemove(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'the category is deleted successfully',
+                })
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'the category not found' })
+            }
         })
         .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false,
-            })
+            return res.status(400).json({ success: false, error: err })
         })
 })
 
