9b6f211534adce28209f8165063c7faed1ab396e
Karim Atif <101069884+Karimatif33@users.noreply.github.com>
2023-04-30 05:59:17 +0300

    EshopeNodeJsProject(2)

--- E-Commerce Web API/app.js
+++ E-Commerce Web API/app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello My First Project')
 })
 
+
 app.listen(port, () => {
     console.log('server is running http://localhost:3000')
 })
+   
+console.log()
