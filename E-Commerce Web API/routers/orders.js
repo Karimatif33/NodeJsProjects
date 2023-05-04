b426391d875c04f34ab899f7a34bbc89c266ed84
Karim Atif <101069884+Karimatif33@users.noreply.github.com>
2023-05-04 18:57:09 +0300

    EshopeNodeJsProject(20)

--- E-Commerce Web API/routers/orders.js
+++ E-Commerce Web API/routers/orders.js
@@ -44,6 +44,19 @@ router.post(`/`, async (req, res) => {
         })
     )
     const orderItemsIdsResolved = await orderItemsIds
+
+    const totalPrices = await Promise.all(
+        orderItemsIdsResolved.map(async (orderItemsId) => {
+            const orderItem = await OrderItem.findById(orderItemsId).populate(
+                'product',
+                'price'
+            )
+            const totalPrice = orderItem.product.price * orderItem.quantity
+            return totalPrice
+        })
+    )
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0)
+    console.log(totalPrice)
     let order = new Order({
         orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -53,7 +66,7 @@ router.post(`/`, async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     })
     order = await order.save()
@@ -61,4 +74,58 @@ router.post(`/`, async (req, res) => {
     res.send(order)
 })
 
+router.put(`/:id`, async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+
+        {
+            status: req.body.status,
+        },
+        { new: true }
+    )
+    if (!order) return res.status(404).send('the order cannot be ubdated!')
+    res.send(order)
+})
+
+router.delete(`/:id`, (req, res) => {
+    Order.findByIdAndRemove(req.params.id)
+        .then(async (order) => {
+            if (order) {
+                await order.orderItems.map(async (orderitem) => {
+                    await OrderItem.findByIdAndRemove(orderitem)
+                })
+                return res.status(200).json({
+                    success: true,
+                    message: 'the order is deleted successfully',
+                })
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'the order not found' })
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err })
+        })
+})
+
+router.get(`/get/totalsals`, async (req, res) => {
+    const totalsals = await Order.aggregate([
+        { $group: { _id: null, totalsales: { $sum: '$totalPrice' } } },
+    ])
+    if (!totalsals) {
+        return res.status(400).send('The order sales cannot be geneated')
+    }
+    res.send({ totalsals: totalsals.pop().totalsales })
+})
+
+router.get(`/get/count`, async (req, res) => {
+    const orderCount = await Order.countDocuments({})
+    if (!orderCount) {
+        res.status(500).json({
+            success: false,
+        })
+    }
+    res.send({ orderCount: orderCount })
+})
 module.exports = router
